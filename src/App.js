660ab62f88e5a08364430e8973f94385eb8c8e45
Robert Godlewski <robtechg5@gmail.com>
2022-03-29 21:27:54 -0700

    Decorator Page.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  useParams
 } from 'react-router-dom';
 import './App.css';
 
@@ -12,12 +13,27 @@ const Home = () => {
   );
 }
 
+const RouteDecour = (props) => {
+  const {info, font_color, bg_color} = useParams();
+
+  return (
+    <h1 style={{
+      color: font_color,
+      backgroundColor: bg_color
+    }}>{isNaN(info) ? "The word is:" : "The number is:"} {info}</h1>
+  );
+}
+
 function App() {
+  const [info, font_color, bg_color] = useState("");
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/:info" element={<RouteDecour info={info} font_color={"black"} bg_color={"black"}/>} />
+          <Route path="/:info/:font_color/:bg_color" element={<RouteDecour info={info} font_color={font_color} bg_color={bg_color}/>} />
         </Routes>
       </BrowserRouter>
     </div>
